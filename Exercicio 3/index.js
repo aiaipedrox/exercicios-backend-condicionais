a93c70fc06f9f10a0fac6ffbcce8543824108f82
Pedro Miranda <pedromirandalino@icloud.com>
2023-06-27 19:32:49 -0300

    Exercicio 3 Finalizado - Domino

---
+++ Exercicio 3/index.js
@@ -0,0 +1,15 @@
+//# Exercício 03
+//## Dominó
+//Num jogo de dominó toda pedra tem dois números. Quando uma pedra tem o mesmo número dos dois lados, dizemos que essa pedra é uma bucha. Faça um programa que imprima "SIM" caso a pedra seja uma bucha e "NÃO" caso contrário.
+//```javascript
+
+
+const ladoA = 3;
+const ladoB = 3;
+//seu código aqui
+
+if (ladoA === ladoB) {
+    console.log("Sim")
+} else {
+    console.log("Nao")
+}
