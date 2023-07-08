db4aa0871dcb55668f8df1c7c810e65038e2aa28
Pedro Miranda <pedromirandalino@icloud.com>
2023-07-08 14:00:50 -0300

    Exercicio 5 Finalizado - Peneira

---
+++ Exercicio 5/index.js
@@ -0,0 +1,16 @@
+//![](https://i.imgur.com/xG74tOh.png)
+//# Exercício 05
+//## Peneira de Vôlei
+//Numa "peneira"(processo seletivo para novos jogadores) de vôlei, em alguns clubes, o primeiro filtro é ver se um determinado candidato tem a altura mínima necessária de 180cm.Faça um programa que, dada a altura de um determinado candidato, diga se ele está aprovado ou reprovado nessa primeira fase do processo.
+//Imprima na tela ** APROVADO ** caso o jogador tenha 180cm de altura ou mais
+//Imprima na tela ** REPROVADO ** caso o jogador tenha menos de 180cm
+
+
+
+const alturaEmCm = 169;
+if (alturaEmCm < 180) {
+    console.log("REPROVADO")
+} else {
+    console.log("APROVADO")
+}
+
