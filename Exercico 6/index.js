6372150d1771ceb4d1fc975f7ba2d644a14b0dc5
Pedro Miranda <pedromirandalino@icloud.com>
2023-07-08 14:12:34 -0300

    Exercicio 6 Finalizado- Definindo as posicoes

---
+++ Exercico 6/index.js
@@ -0,0 +1,27 @@
+//![](https://i.imgur.com/xG74tOh.png)
+//# Exercício 06
+//## Definindo as posições
+
+//Após passar na primeira fase de uma peneira de vôlei, os candidatos aprovados são separados em grupos diferentes, de acordo com suas alturas, para serem testados para posições específicas. Faça um programa que, dada a altura de um determinado candidato, diga para qual posição ele deverá fazer o teste.
+
+//Caso o jogador tenha menos de 180cm, o programa deve imprimir **REPROVADO**
+//Caso tenha entre 180cm e 185cm, o programa deve imprimir **LÍBERO**
+//Caso tenha entre 186cm e 195cm, o programa deve imprimir **PONTEIRO**
+//Caso tenha entre 196cm e 205cm, o programa deve imprimir **OPOSTO**
+//Caso tenha mais de 205cm, o programa deve imprimir **CENTRAL**
+
+
+
+const alturaEmCm = 207;
+
+if (alturaEmCm < 180) {
+    console.log('REPROVADO');
+} else if (alturaEmCm <= 185) {
+    console.log('LIBERO');
+} else if (alturaEmCm <= 195) {
+    console.log('PONTEIRO');
+} else if (alturaEmCm <= 205) {
+    console.log('OPOSTO');
+} else {
+    console.log('CENTRAL');
+}
