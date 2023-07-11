16c17d4d6a91cc3528d3b3bcb5bffcb1e038dda6
Pedro Miranda <pedromirandalino@icloud.com>
2023-07-11 16:01:46 -0300

    Exercicio 15 Finalizdo - Nome para exibição

---
+++ Exercicio 15/index.js
@@ -0,0 +1,13 @@
+const primeiroNome = "Pedro";
+const sobrenome = "Miranda";
+const apelido = "";
+
+//seu código aqui
+
+if (apelido) {
+    console.log(apelido);
+} else if (sobrenome) {
+    console.log(`${primeiroNome} ${sobrenome}`);
+} else {
+    console.log(primeiroNome);
+}
