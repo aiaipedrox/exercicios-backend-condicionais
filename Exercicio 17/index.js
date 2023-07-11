50ebf1f17525224764949fc78b9cecf608f156e0
Pedro Miranda <pedromirandalino@icloud.com>
2023-07-11 16:35:27 -0300

    Exercicio 17 Finalizado

---
+++ Exercicio 17/index.js
@@ -0,0 +1,15 @@
+//valor do produto comprado.
+const valorDoProduto = 100000;
+
+//quantidade de parcelas
+const quantidadeDoParcelamento = 10;
+
+//valor pago
+const valorPago = 300;
+
+//Restam 7 parcelas de R$100
+
+const valorParcelaEmReais = (valorDoProduto / quantidadeDoParcelamento) / 100;
+const parcelasPagas = valorPago / valorParcelaEmReais;
+
+console.log(`Restam ${quantidadeDoParcelamento - parcelasPagas} parcelas de R$${valorParcelaEmReais.toFixed(2)}`);
